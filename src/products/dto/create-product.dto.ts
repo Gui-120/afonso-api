0d12533b85fc8888b419f7c660d278e1e8eb9887
Gui-120 <guimega120@gmail.com>
2025-06-03 21:55:12 -0300

    ajustes api

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsNumber, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
---
+++ src/products/products.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
+import { Product, ProductSchema } from './schemas/product.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])
+  ],
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
+export class ProductsModule {}
